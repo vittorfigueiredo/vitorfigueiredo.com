4f45115800e7e3ff62911d2daf49ae645dcb0528
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2022-12-09 11:28:21 -0300

    Add button to up page

--- pages/home.php
+++ pages/home.php
@@ -117,6 +117,9 @@
         <a href="https://twitter.com/ovitorfs" target="_blank"><img src="/public/imgs/twitter.svg" alt="twitter"></a>
       </div>
     </div>
+    <div id="up-arrow">
+      <img src="/public/imgs/chevron-up.svg" alt="up" onclick="scrollUp()">
+    </div>
   </footer>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   <script src="/public/js/main.js"></script>
