1482a66ceb5c35c730394e956b13d034f54c6708
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2023-01-04 12:45:03 -0300

    Add bootstrap script

--- app/views/master.php
+++ app/views/master.php
@@ -75,6 +75,8 @@
       <p>Developed by<a href="https://github.com/vittorfigueiredo" target="_blank">Vitor Figueiredo</a></p>
     </div>
   </footer>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   <script src="/assets/js/main.js"></script>
 </body>
 </html>
