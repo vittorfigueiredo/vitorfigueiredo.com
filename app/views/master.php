0bcfba334fe1dd84da0778da5d309f307d4ae052
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2023-01-04 12:29:57 -0300

    Change favicon

--- app/views/master.php
+++ app/views/master.php
@@ -15,7 +15,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="shortcut icon" href="/assets/img/favicon.png" type="image/x-icon">
+  <link rel="shortcut icon" href="/assets/img/favicon.ico" type="image/x-icon">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
   <link rel="stylesheet" href="/assets/css/style.css">
   <title><?=$this->e($title)?></title>
@@ -74,7 +74,6 @@
       <p>Developed by<a href="https://github.com/vittorfigueiredo" target="_blank">Vitor Figueiredo</a></p>
     </div>
   </footer>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   <script src="/assets/js/main.js"></script>
 </body>
 </html>
