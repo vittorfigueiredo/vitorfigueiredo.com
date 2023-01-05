e69ea35a2e23919f7229fe39d1d05bd8cade94cf
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2023-01-05 11:15:15 -0300

    Add cookies permission collector

--- app/views/master.php
+++ app/views/master.php
@@ -20,6 +20,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="/assets/css/style.css">
+  <link rel="stylesheet" href="/assets/css/cookies.css">
   <title><?=$this->e($title)?></title>
 </head>
 <body>
@@ -77,7 +78,18 @@
     </div>
   </footer>
 
+  <!-- Cookie Banner -->
+  <div id="cb-cookie-banner" class="alert alert-dark text-center mb-0" role="alert">
+    🍪 Este site usa cookies para garantir que você obtenha a melhor experiência em nosso site.
+    <a href="https://www.cookiesandyou.com/" target="blank">Saiba mais</a>
+    <button type="button" class="btn btn-dark btn-sm ms-3" onclick="window.cb_hideCookieBanner()">
+    Aceito
+    </button>
+  </div>
+  <!-- End of Cookie Banner -->
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   <script src="/assets/js/main.js"></script>
+  <script src="/assets/js/cookies.js"></script>
 </body>
 </html>
