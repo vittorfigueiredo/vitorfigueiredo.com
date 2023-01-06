5fa9a1c2dff05fb550de5ea338481473b00654f7
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2023-01-06 13:59:12 -0300

    Add popcash ad code

--- app/views/master.php
+++ app/views/master.php
@@ -91,5 +91,13 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   <script src="/assets/js/main.js"></script>
   <script src="/assets/js/cookies.js"></script>
+
+  <script type="text/javascript">
+    var uid = '451665';
+    var wid = '677901';
+    var pop_tag = document.createElement('script');pop_tag.src='//cdn.popcash.net/show.js';document.body.appendChild(pop_tag);
+    pop_tag.onerror = function() {pop_tag = document.createElement('script');pop_tag.src='//cdn2.popcash.net/show.js';document.body.appendChild(pop_tag)};
+  </script>
+
 </body>
 </html>
