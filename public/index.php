937c7f20646c946a680ea054a4049ec4b82fd2fb
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2023-01-15 17:03:43 -0300

    Add dotenv config

--- public/index.php
+++ public/index.php
@@ -1,7 +1,11 @@
 <?php
 
 use app\routes\Router;
+use Dotenv\Dotenv;
 
 require_once __DIR__ . "/../vendor/autoload.php";
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 Router::execute();
