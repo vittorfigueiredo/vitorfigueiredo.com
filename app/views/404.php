6cec0a90bc306b4c2f5d9d1f59f9e60e6d342b18
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2023-01-15 05:19:26 -0300

    Adjusts on style

--- app/views/404.php
+++ app/views/404.php
@@ -1,6 +1,6 @@
 <?php $this->layout("master", ["title" => $title]) ?>
 
-<div style="display: flex; flex-direction: column; text-align: center; margin: 6rem 0; gap: 2rem;">
-  <h1 style="font-size: 4rem;">Oops! :(</h1>
-  <p>Aparentemente a página que você está tentando acessar não está mais aqui.</p>
+<div style="display: flex; flex-direction: column; justify-content: center; text-align: center; margin: 6rem 0; gap: 1rem;">
+  <h1 style="font-size: 4rem; margin: 0;">Oops! :(</h1>
+  <p style="font-size: 1.5rem; margin: 0;">Aparentemente a página que você está tentando acessar não está mais aqui.</p>
 </div>
