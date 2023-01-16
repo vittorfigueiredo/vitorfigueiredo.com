de94a38f4cda1c4a1fca3b34447550083618ad6c
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2023-01-15 21:46:29 -0300

    Load dotenv

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ use Dotenv\Dotenv;
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
-$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv::createImmutable(__DIR__ . "./../");
 $dotenv->load();
 
 Router::execute();
