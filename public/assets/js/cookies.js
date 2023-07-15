91854cd934bdaae146db0c8cc4506e7e69cdebc9
Vitor <vitorfigueiredo855@gmail.com>
2023-07-15 06:15:04 -0300

    adjust: improve indentations

--- public/assets/js/cookies.js
+++ public/assets/js/cookies.js
@@ -1,30 +1,29 @@
 /* Javascript to show and hide cookie banner using localstorage */
 /* Shows the Cookie banner */
-function showCookieBanner(){
+function showCookieBanner() {
   let cookieBanner = document.getElementById("cb-cookie-banner");
   cookieBanner.style.display = "block";
- }
+}
 
- /* Hides the Cookie banner and saves the value to localstorage */
- function hideCookieBanner(){
+/* Hides the Cookie banner and saves the value to localstorage */
+function hideCookieBanner() {
   localStorage.setItem("cb_isCookieAccepted", "yes");
   let cookieBanner = document.getElementById("cb-cookie-banner");
   cookieBanner.style.display = "none";
- }
+}
 
- /* Checks the localstorage and shows Cookie banner based on it. */
- function initializeCookieBanner(){
+/* Checks the localstorage and shows Cookie banner based on it. */
+function initializeCookieBanner() {
   let isCookieAccepted = localStorage.getItem("cb_isCookieAccepted");
-  if(isCookieAccepted === null)
-  {
-   localStorage.setItem("cb_isCookieAccepted", "no");
-   showCookieBanner();
+  if (isCookieAccepted === null) {
+    localStorage.setItem("cb_isCookieAccepted", "no");
+    showCookieBanner();
   }
-  if(isCookieAccepted === "no"){
-   showCookieBanner();
+  if (isCookieAccepted === "no") {
+    showCookieBanner();
   }
- }
+}
 
- // Assigning values to window object
- window.onload = initializeCookieBanner();
- window.cb_hideCookieBanner = hideCookieBanner;
+// Assigning values to window object
+window.onload = initializeCookieBanner();
+window.cb_hideCookieBanner = hideCookieBanner;
