9cd1d95fbb4f1c7c37ece4e1773ee6e2d4ba1285
Vitor <vitorfigueiredo855@gmail.com>
2023-07-16 04:31:15 -0300

    adjust: remove div container

--- app/views/article.php
+++ app/views/article.php
@@ -2,45 +2,43 @@
 
 <link rel="stylesheet" href="/assets/css/article.css">
 
-<div class="container">
-  <class class="title">
-    <h1><?=$this->e($article["title"]) ? $this->e($article["title"]) : "Lorem ipsum dolor, sit amet consectetur adipisicing elit."?></h1>
-  </class>
-  <div class="post-info">
-    <div class="author">
-      <h6>Por Vitor Figueiredo, <?=$this->e($article["createdAt"]) ? date_format(new DateTime($this->e($article["createdAt"])), 'd/m/Y') : "06 Jan 2023" ?></h6>
-    </div>
+<class class="title">
+  <h1><?=$this->e($article["title"]) ? $this->e($article["title"]) : "Lorem ipsum dolor, sit amet consectetur adipisicing elit."?></h1>
+</class>
+<div class="post-info">
+  <div class="author">
+    <h6>Por Vitor Figueiredo, <?=$this->e($article["createdAt"]) ? date_format(new DateTime($this->e($article["createdAt"])), 'd/m/Y') : "06 Jan 2023" ?></h6>
   </div>
-  <!-- <div class="tags">
-    <a href="#">PHP</a>
-  </div> -->
+</div>
+<!-- <div class="tags">
+  <a href="#">PHP</a>
+</div> -->
 
-  <div class="content">
-    <?php
-      use Parsedown;
-      use app\helpers\Uri;
+<div class="content">
+  <?php
+    use Parsedown;
+    use app\helpers\Uri;
 
-      $name = addslashes(str_replace("name=", "", Uri::get("query")));
+    $name = addslashes(str_replace("name=", "", Uri::get("query")));
 
-      $article = file_get_contents("articles/$name.md");
+    $article = file_get_contents("articles/$name.md");
 
-      if (!$article) {
-        header("Location: /ArticleNotFound");
-      }
+    if (!$article) {
+      header("Location: /ArticleNotFound");
+    }
 
-      $parser = new Parsedown();
-      echo $parser->text($article);
-    ?>
+    $parser = new Parsedown();
+    echo $parser->text($article);
+  ?>
+</div>
+
+<div class="about-author border border-secondary">
+  <div class="author-image">
+    <img height="128" width="128" src="/assets/img/profile.jpg" alt="author image">
   </div>
-
-  <div class="about-author border border-secondary">
-    <div class="author-image">
-      <img height="128" width="128" src="/assets/img/profile.jpg" alt="author image">
-    </div>
-    <div class="about-author-description">
-      <h1 class="text-light">Sobre Vitor</h1>
-      <p class="text-light">Desenvolvedor Web Backend, apaixonado por tecnologia e gamer nas hora vagas.</p>
-    </div>
+  <div class="about-author-description">
+    <h1 class="text-light">Sobre Vitor</h1>
+    <p class="text-light">Desenvolvedor Web Backend, apaixonado por tecnologia e gamer nas hora vagas.</p>
   </div>
 </div>
 
