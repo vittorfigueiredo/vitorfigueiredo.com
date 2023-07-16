19bb3d73025cdc4cf85e2a0a4971e8dd3ea24179
Vitor <vitorfigueiredo855@gmail.com>
2023-07-16 04:30:44 -0300

    adjust: align div center in vertical

--- app/views/404.php
+++ app/views/404.php
@@ -1,6 +1,18 @@
 <?php $this->layout("master", ["title" => $title]) ?>
 
-<div style="display: flex; flex-direction: column; justify-content: center; text-align: center; margin: 6rem 0; gap: 1rem;">
+<style>
+  #message404 {
+    position: relative;
+    display: flex;
+    flex-direction:column;
+    justify-content: center;
+    min-height: 65vh;
+    text-align: center;
+    gap: 1rem;
+  }
+</style>
+
+<div id="message404">
   <h1 style="font-size: 4rem; margin: 0;">Oops! :(</h1>
   <p style="font-size: 1.5rem; margin: 0;">Aparentemente a página que você está tentando acessar não está mais aqui.</p>
 </div>
