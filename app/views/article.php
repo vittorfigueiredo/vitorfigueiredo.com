e6ee3dfb4adaeb1c2816b39f081fc9106312b779
Vitor <vitorfigueiredo855@gmail.com>
2023-08-17 02:36:25 -0300

    fix: remove unused script

--- app/views/article.php
+++ app/views/article.php
@@ -42,6 +42,5 @@
   </div>
 </div>
 
-<script src="/assets/js/article.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js"></script>
 <script>hljs.highlightAll()</script>
