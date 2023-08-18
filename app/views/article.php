e438c7c2166cb553c9b5604ebff57bf05271c61a
Vitor <vitorfigueiredo855@gmail.com>
2023-08-17 21:41:00 -0300

    feat: removed about author section and added comments section

--- app/views/article.php
+++ app/views/article.php
@@ -32,15 +32,43 @@
   ?>
 </div>
 
-<div class="about-author border border-secondary">
-  <div class="author-image">
-    <img height="128" width="128" src="/assets/img/profile.webp" alt="author image">
-  </div>
-  <div class="about-author-description">
-    <h1 class="text-light">Sobre Vitor</h1>
-    <p class="text-light">Desenvolvedor Web Backend, apaixonado por tecnologia e gamer nas hora vagas.</p>
-  </div>
+<div id="comments-section" class="mt-5">
+  <div id="disqus_recommendations"></div>
+  <div id="disqus_thread"></div>
 </div>
+
+<script>
+(function() { // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
+var d = document, s = d.createElement('script'); // IMPORTANT: Replace EXAMPLE with your forum shortname!
+s.src = 'https://vitor-figueiredo.disqus.com/recommendations.js'; s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
+</script>
+<noscript>
+Please enable JavaScript to view the
+<a href="https://disqus.com/?ref_noscript" rel="nofollow">
+comments powered by Disqus.
+</a>
+</noscript>
+
+<script>
+    /**
+    *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+    *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+    /*
+    var disqus_config = function () {
+    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    };
+    */
+    (function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://vitor-figueiredo.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
 
 <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js"></script>
 <script>hljs.highlightAll()</script>
