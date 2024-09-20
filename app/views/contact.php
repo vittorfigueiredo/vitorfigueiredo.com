d8764f6af9356eb3dfeebcf9bef6445d750497ec
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2024-09-20 18:42:34 -0300

    refactor: remove YouTube videos section from contact page

--- app/views/contact.php
+++ app/views/contact.php
@@ -25,18 +25,4 @@
     <h5 class="text-center mb-4">Minha Playlist 🤘</h5>
     <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/4i9BN9JErxaKZAPZmri873?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
   </div>
-
-  <div id="youtube-videos" class="mt-4">
-    <?php $videosInfo = json_decode(file_get_contents("https://www.googleapis.com/youtube/v3/search?key=".$_ENV["YOUTUBE_API_ACCESS_KEY"]."&channelId=".$_ENV["YOUTUBE_CHANNEL_ID"]."&part=snippet,id&order=date&maxResults=5"), true); ?>
-
-    <?php if (!empty($videosInfo)) { ?>
-
-      <h5 class="text-center">Videos Cools 😎</h5>
-
-      <?php foreach ($videosInfo["items"] as $videoInfo) { ?>
-        <iframe class="mt-4" width="100%" height="720" src="https://www.youtube.com/embed/<?php echo $videoInfo["id"]["videoId"] ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-      <?php } ?>
-
-    <?php } ?>
-  </div>
 </div>
