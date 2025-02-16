4a31b7875e92f1bea6ddae2964d5a9e012076a6d
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2025-02-16 14:02:46 -0300

    style: update headings and rearrange social media links in contact.php

--- app/views/contact.php
+++ app/views/contact.php
@@ -11,18 +11,17 @@
 <div class="d-flex flex-column justify-content-center mt-4">
   <div id="about-section" class="d-flex flex-column gap-3">
     <img class="rounded-circle mx-auto img-responsive" src="assets/img/profile.webp" alt="profile picture" height="125px" width="125px">
-    <h5 class="text-center">Vitor Figueiredo</h5>
+    <h2 class="text-center">Vitor Figueiredo</h2>
   </div>
 
   <div id="links-section" class="d-flex flex-column gap-3 mt-4">
-    <a class="btn btn-secondary text-decoration-none" href="https://github.com/vittorfigueiredo" target="_blank"><i class="bi bi-github"></i> GitHub</a>
-    <a class="btn btn-secondary text-decoration-none" href="https://vitorfigueiredo.com" target="_blank"><i class="bi bi-browser-safari"></i> Website</a>
-    <a class="btn btn-secondary text-decoration-none" href="https://www.linkedin.com/in/vittorfigueiredo" target="_blank"><i class="bi bi-linkedin"></i> Linkedin</a>
-    <a class="btn btn-secondary text-decoration-none" href="https://www.youtube.com/@vittorfigueiredo" target="_blank"><i class="bi bi-youtube"></i> YouTube</a>
+    <a class="btn btn-secondary text-decoration-none" href="https://www.instagram.com/vittorfigueiredo"><i class="bi bi-instagram"></i> Instagram</a>
+    <a class="btn btn-secondary text-decoration-none" href="https://www.linkedin.com/in/vittorfigueiredo"><i class="bi bi-linkedin"></i> Linkedin</a>
+    <a class="btn btn-secondary text-decoration-none" href="https://github.com/vittorfigueiredo"><i class="bi bi-github"></i> GitHub</a>
   </div>
 
   <div id="spotify-playlist" class="mt-4">
-    <h5 class="text-center mb-4">Minha Playlist 🤘</h5>
+    <h2 class="text-center mb-4 fs-3">Minha Playlist 🤘</h2>
     <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/4i9BN9JErxaKZAPZmri873?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
   </div>
 </div>
